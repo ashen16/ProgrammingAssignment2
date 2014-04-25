aab9f05eb7483685dc11da1700db157d8f9839c8
Amanda Shen <ashen16@yahoo.com>
2014-04-25 18:26:19 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,12 @@
-## The purpose of these two function is trying to 
-## use cache to avoid unnecessary repeated computation 
-## and save computation resources
-##################################################################
+## The purpose of these two function is trying to use cache technique to avoid unnecessary repeated computation 
+##and save computation resources
+########################################################################################################################
 
-## The purpose of this function is to create a special "matrix" object 
-####################################################################
+## The purpose of this function is to create a special "matrix" object which can be cached
+########################################################################################################################
 
 makeCacheMatrix <- function(x = matrix()) {
+
   inverse <- NULL
   set <- function(y) {
     x <<- y
@@ -23,13 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function in another way is to compute the inverse of the special "matrix" 
-## created by the function above. If the inverse has already been calculated
-## (and the matrix has not changed), the function should retrieve the inverse from the cache.
-########################################################################
+## This function in another way is to compute the inverse of the special "matrix" created by the function above. 
+##If the inverse has already been calculated (and the matrix has not changed), the function should retrieve the inverse 
+##from the cache.
+######################################################################################################################
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
   inverse <- x$getinverse()
   if(!is.null(inverse)) {
     print ("getting cached data")
@@ -38,7 +38,7 @@ cacheSolve <- function(x, ...) {
   data <- x$get()
   inverse <- solve(data, ...)
   x$setinverse(inverse)
-  print (inverse)
+  inverse    ## Return a matrix that is the inverse of 'x'
   
 }
 
